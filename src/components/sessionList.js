2cc01f0d016185ac7fcbceab701abe86d81a959d
Lucas Freitas <lucaslcfjf@hotmail.com>
2019-11-28 15:24:58 -0300

    Alterações no layout

--- src/components/sessionList.js
+++ src/components/sessionList.js
@@ -3,13 +3,26 @@ import '../css/sessionList.css';
 
 class sessionList extends Component{
 
+    selecionaTipo(evento){
+        console.log(evento.target.id);
+        if(evento.target.id == "tipoTodos"){
+            document.getElementById(evento.target.id).style.borderBottom = "1px solid #ffffff";
+            document.getElementById("tipoFavoritos").style.borderBottom = "1px solid #8f1616";
+        } else {
+            document.getElementById(evento.target.id).style.borderBottom = "1px solid #ffffff";
+            document.getElementById("tipoTodos").style.borderBottom = "1px solid #8f1616";
+        }
+    }
 
     render(){
         return (
             <div id="sessionList">
                 <div className="header">
                     <h1 className="titleSession">Atividades</h1>
-                    <h2>A subtitle for your page goes here</h2>
+                    <ul className="listaTipos">
+                        <li className="tipos" id="tipoTodos" onClick={this.selecionaTipo}>TODOS</li>
+                        <li className="tipos" id="tipoFavoritos" onClick={this.selecionaTipo}>FAVORITOS</li>
+                    </ul>
                 </div>
 
                 <div className="content">
