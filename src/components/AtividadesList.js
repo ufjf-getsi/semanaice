5ff535c1d795e33423389e4ecce2a1b5ec436d44
Lucas Freitas <lucaslcfjf@hotmail.com>
2019-12-16 17:42:38 -0300

    Implementação da opção favoritar (a corrigir)

--- src/components/AtividadesList.js
+++ src/components/AtividadesList.js
@@ -2,23 +2,36 @@ import React, { Component } from 'react';
 import Session from '../data/sessions';
 import AtividadesListItem from './AtividadesListItem';
 import '../css/AtividadesList.css';
+import PubSub from 'pubsub-js';
 
 class AtividadesList extends Component{
 
     constructor(){
         super();
-        this.state = {atividades : Session};
+        this.state = {atividades : Session, selecFav : false};
     
         
+    }
+
+    componentDidMount(){
+        PubSub.subscribe('atualizaFavoritos', function(topico, novaLista){
+            if(this.state.selecFav){
+                this.setState({atividades : novaLista});
+            }
+        }.bind(this));
     }
 
     selecionaTipo(evento){
         if(evento.target.id === "tipoTodos-Atividades"){
             document.getElementById(evento.target.id).style.borderBottom = "1px solid #ffffff";
             document.getElementById("tipoFavoritos-Atividades").style.borderBottom = "1px solid #8f1616";
+
+            this.setState({atividades : Session, selecFav : false});
         } else {
             document.getElementById(evento.target.id).style.borderBottom = "1px solid #ffffff";
             document.getElementById("tipoTodos-Atividades").style.borderBottom = "1px solid #8f1616";
+
+            this.setState({atividades : JSON.parse(localStorage.getItem("favoritos")), selecFav : true});
         }
     }
 
@@ -28,16 +41,32 @@ class AtividadesList extends Component{
                 <div className="header-Atividades">
                     <h1 className="title-Atividades">Atividades</h1>
                     <ul className="listaTipos-Atividades">
-                        <li className="tipos-Atividades" id="tipoTodos-Atividades" onClick={this.selecionaTipo}>TODOS</li>
-                        <li className="tipos-Atividades" id="tipoFavoritos-Atividades" onClick={this.selecionaTipo}>FAVORITOS</li>
+                        <li className="tipos-Atividades" id="tipoTodos-Atividades" onClick={this.selecionaTipo.bind(this)}>TODOS</li>
+                        <li className="tipos-Atividades" id="tipoFavoritos-Atividades" onClick={this.selecionaTipo.bind(this)}>FAVORITOS</li>
                     </ul>
                 </div>
 
                 <div className="content-Atividades">
                     {this.state.atividades.map(function(item){
-                        return (
-                            <AtividadesListItem key={item.id} id={item.id} nome={item.name} dataInicio={item.dateTimeStart} dataFinal={item.dateTimeEnd} local={item.location} atividade={item}/>
-                        );
+                        var favoritos = JSON.parse(localStorage.getItem("favoritos"));
+                        var existe = false;
+                        if(favoritos != null) {
+                            favoritos.map(function(fav){
+                                if(item.id === fav.id){
+                                    existe = true;
+                                }
+                                return(null);
+                            })
+                        }
+                        if(!existe){
+                            return (
+                                <AtividadesListItem key={item.id} fav={false} id={item.id} nome={item.name} dataInicio={item.dateTimeStart} dataFinal={item.dateTimeEnd} local={item.location} atividade={item}/>
+                            );
+                        } else {
+                            return (
+                                <AtividadesListItem key={item.id} fav={true} id={item.id} nome={item.name} dataInicio={item.dateTimeStart} dataFinal={item.dateTimeEnd} local={item.location} atividade={item}/>
+                            );
+                        }
                     })}
                 </div>
             </div>
--- src/components/AtividadesListItem.js
+++ src/components/AtividadesListItem.js
@@ -1,30 +1,67 @@
 import React, { Component } from 'react';
 import '../css/AtividadesListItem.css';
+import PubSub from 'pubsub-js';
 
 class AtividadesListItem extends Component{
 
+    componentDidMount(){
+        if(this.props.fav){
+            document.getElementById(this.props.id).style.background = "#d61f1f";
+            document.getElementById(this.props.id).style.color = "#ffffff";
+        } else {
+            document.getElementById(this.props.id).style.background = "#ffffff";
+            document.getElementById(this.props.id).style.color = "#000000";
+        }
+    }
 
-
-    favoritar(evento){
+    favoritar(evento){    
         if(localStorage.getItem("favoritos") != null){
             var favoritos = JSON.parse(localStorage.getItem("favoritos"));
-            favoritos.push(this.props.atividade);
-            localStorage.setItem("favoritos", JSON.stringify(favoritos));
+            var atual = this.props.atividade;
+            var existe = false;
+            favoritos.map(function(item){
+                if(item.id === atual.id){
+                    existe = true;
+                }
+                return(null);
+            })
+            if(!existe){
+                favoritos.push(this.props.atividade);
+                localStorage.setItem("favoritos", JSON.stringify(favoritos));
+
+                document.getElementById(this.props.id).style.background = "#d61f1f";
+                document.getElementById(this.props.id).style.color = "#ffffff";
+            } else {
+                var novoFavoritos = [];
+                favoritos.map(function(item){
+                    if(item.id !== atual.id){
+                        novoFavoritos.push(item);
+                    }
+                })
+                localStorage.setItem("favoritos", JSON.stringify(novoFavoritos));
+                PubSub.publish('atualizaFavoritos', novoFavoritos);
+
+                document.getElementById(this.props.id).style.background = "#ffffff";
+                document.getElementById(this.props.id).style.color = "#000000";
+            }
         } else {
             var favoritos = [];
             favoritos.push(this.props.atividade);
             localStorage.setItem("favoritos", JSON.stringify(favoritos));
+
+            document.getElementById(this.props.id).style.background = "#d61f1f";
+            document.getElementById(this.props.id).style.color = "#ffffff";
         }
     }
 
 
     render(){
         return (
-            <div id="item-AtividadesItem" key={this.props.id}>
+            <div id="item-AtividadesItem">
                 <p className="titulo-AtividadesItem">{this.props.nome}</p>
                 <p className="horarioLocal-AtividadesItem">{this.props.dataInicio} - {this.props.dataFinal}: {this.props.local}</p>
 
-                <button className="favoritar-AtividadesItem" id={"fav-" + this.props.id} onClick={this.favoritar.bind(this)}><span>Favoritar </span></button>
+                <button className="favoritar-AtividadesItem" id={this.props.id} onClick={this.favoritar.bind(this)}>Favoritar </button>
             </div>
         );
     }
